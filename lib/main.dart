c02b89b3892a2700be82b5b52e91369f2ee30b3a
Mohit Choudhary <53377268+MohitChoudhary23@users.noreply.github.com>
2021-07-15 12:39:54 +0530

    Add animatin

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+// @dart=2.9
+import 'animatedCheck.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rounded Loading Button Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  //final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+
+    _animation = new Tween<double>(begin: 0, end: 1).animate(
+        new CurvedAnimation(
+            parent: _animationController, curve: Curves.easeInOutCirc));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Animated Check Example"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  child: AnimatedCheck(
+                progress: _animation,
+                size: 200,
+                color:Colors.greenAccent,
+              )),
+              TextButton(
+                  child: Text("Check"),
+                  onPressed: _animationController.forward),
+              TextButton(
+                  child: Text("Reset"), onPressed: _animationController.reset)
+            ],
+          ),
+        ));
+  }
+}
